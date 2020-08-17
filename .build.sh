1250af25e22079aed858a3843632b4daf9f62e32
Stephan Grein <grein@temple.edu>
2020-08-17 21:54:43 +0200

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -10,3 +10,6 @@ for i in "${cells[@]}"; do
   ${TRAVIS_BUILD_DIR}/travis_root/ug4/bin/ugshell -call "test_import_swc_and_regularize_var(\"${i}.CNG.swc\")" &> /dev/null
  # ${TRAVIS_BUILD_DIR}/travis_root/ug4/bin/ugshell -call "test_import_swc_general_var(\"new_strategy.swc\", false, 0.5, true, 8,1, true, 1.0, false, false, \"identity\", -1)" &> /dev/null
 done
+
+# echo "Testing Y-branches.."
+# ${TRAVIS_BUILD_DIR}/travis_root/ug4/bin/ugshell -call "create_branches_from_swc(\"$TRAVIS_BUILD_DIR/grids/Y_angle=90.0.swc\", 0.5, 0)"
