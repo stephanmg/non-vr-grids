24a99c8f8e024d0c15a33c40d8f578b4b6e6c0f5
Stephan Grein <grein@temple.edu>
2020-09-02 17:24:57 +0200

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -7,7 +7,7 @@ echo "Testing non-VR use-case grids..."
 for i in "${cells[@]}"; do
   mkdir -p TestCell/${i}.CNG
   python get_swc.py --name ${i}
-  ${TRAVIS_BUILD_DIR}/travis_root/ug4/bin/ugshell -call "test_import_swc_and_regularize_var(\"${i}.CNG.swc\", 1.0)" 
+  ${TRAVIS_BUILD_DIR}/travis_root/ug4/bin/ugshell -call "test_import_swc_general_var_benchmark(\"${i}.CNG.swc\", false, 0.5, true, 8,1, true, 1.0, false, false, \"identity\", -1)"
  # ${TRAVIS_BUILD_DIR}/travis_root/ug4/bin/ugshell -call "test_import_swc_general_var(\"new_strategy.swc\", false, 0.5, true, 8,1, true, 1.0, false, false, \"identity\", -1)" &> /dev/null
 done
 
